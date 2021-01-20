ce9f16c5190ca054364421954454da851f2ce76c
sa-webb <sawebb30@gmail.com>
2021-01-20 13:26:18 -0500

    feat: MOVE_TASK

--- src/context.tsx
+++ src/context.tsx
@@ -2,8 +2,10 @@ import React, { createContext, useContext, useReducer } from "react";
 import { nanoid } from "nanoid";
 import {
   findItemIndexById,
+  insertItemAtIndex,
   moveItem,
   overrideItemAtIndex,
+  removeItemAtIndex,
 } from "./utils/arrayUtils";
 import { DragItem } from "./types/DragItem";
 
@@ -21,6 +23,15 @@ type Action =
       payload: {
         dragIndex: number;
         hoverIndex: number;
+      };
+    }
+  | {
+      type: "MOVE_TASK";
+      payload: {
+        dragIndex: number;
+        hoverIndex: number;
+        sourceColumn: string;
+        targetColumn: string;
       };
     }
   | {
@@ -73,6 +84,51 @@ const appStateReducer = (state: AppState, action: Action): AppState => {
     }
     case "SET_DRAGGED_ITEM": {
       return { ...state, draggedItem: action.payload };
+    }
+    case "MOVE_TASK": {
+      const {
+        dragIndex,
+        hoverIndex,
+        sourceColumn,
+        targetColumn,
+      } = action.payload;
+
+      const sourceListIndex = findItemIndexById(state.lists, sourceColumn);
+
+      const targetListIndex = findItemIndexById(state.lists, targetColumn);
+
+      const sourceList = state.lists[sourceListIndex];
+      const task = sourceList.tasks[dragIndex];
+
+      const updatedSourceList = {
+        ...sourceList,
+        tasks: removeItemAtIndex(sourceList.tasks, dragIndex),
+      };
+
+      const stateWithUpdatedSourceList = {
+        ...state,
+        lists: overrideItemAtIndex(
+          state.lists,
+          updatedSourceList,
+          sourceListIndex
+        ),
+      };
+
+      const targetList = stateWithUpdatedSourceList.lists[targetListIndex];
+
+      const updatedTargetList = {
+        ...targetList,
+        tasks: insertItemAtIndex(targetList.tasks, task, hoverIndex),
+      };
+
+      return {
+        ...stateWithUpdatedSourceList,
+        lists: overrideItemAtIndex(
+          stateWithUpdatedSourceList.lists,
+          updatedTargetList,
+          targetListIndex
+        ),
+      };
     }
     default: {
       return state;
