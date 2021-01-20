75e11db6704f888fac125188eeda96cb12c9bc0b
sa-webb <sawebb30@gmail.com>
2021-01-20 02:07:42 -0500

    feat: move, remove, insert Items

--- src/utils/arrayUtils.ts
+++ src/utils/arrayUtils.ts
@@ -20,3 +20,15 @@ export function overrideItemAtIndex<T>(
   });
 }
 
+export const moveItem = <T>(array: T[], from: number, to: number) => {
+  const item = array[from];
+  return insertItemAtIndex(removeItemAtIndex(array, from), item, to);
+};
+
+export function removeItemAtIndex<T>(array: T[], index: number) {
+  return [...array.slice(0, index), ...array.slice(index + 1)];
+}
+
+export function insertItemAtIndex<T>(array: T[], item: T, index: number) {
+  return [...array.slice(0, index), item, ...array.slice(index)];
+}
