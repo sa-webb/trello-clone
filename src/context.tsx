1dd2e556cf99e508b0664d870a6b1ab2fbaf304c
sa-webb <sawebb30@gmail.com>
2021-01-20 00:03:44 -0500

    initial

---
+++ src/context.tsx
@@ -0,0 +1,55 @@
+import React, { createContext, useContext } from "react";
+
+interface Task {
+  id: string;
+  text: string;
+}
+
+interface List {
+  id: string;
+  text: string;
+  tasks: Task[];
+}
+
+export interface AppState {
+  lists: List[];
+}
+interface AppStateContextProps {
+  state: AppState;
+}
+
+const AppStateContext = createContext<AppStateContextProps>(
+  {} as AppStateContextProps
+);
+
+export const useAppState = () => {
+  return useContext(AppStateContext);
+};
+
+export const AppStateProvider = ({ children }: React.PropsWithChildren<{}>) => {
+  return (
+    <AppStateContext.Provider value={{ state: appData }}>
+      {children}
+    </AppStateContext.Provider>
+  );
+};
+
+const appData: AppState = {
+  lists: [
+    {
+      id: "0",
+      text: "To Do",
+      tasks: [{ id: "c0", text: "Generate app scaffold" }],
+    },
+    {
+      id: "1",
+      text: "In Progress",
+      tasks: [{ id: "c2", text: "Learn Typescript" }],
+    },
+    {
+      id: "2",
+      text: "Done",
+      tasks: [{ id: "c3", text: "Begin to use static typing" }],
+    },
+  ],
+};
